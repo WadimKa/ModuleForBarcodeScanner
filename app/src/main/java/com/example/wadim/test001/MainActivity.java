ceecf217e27372b3e9fbc575856fe871d41738e4
Wadim <waadimka@mail.ru>
2017-06-22 13:34:44 +0300

    22.06
    -fixed error from maiAct
    -added context menu and his items listener

--- app/src/main/java/com/example/wadim/test001/MainActivity.java
+++ app/src/main/java/com/example/wadim/test001/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.wadim.test001;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -13,25 +18,27 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
     String[] arr;
-    EditText editText;
+    EditText editText, edtForRename;
+    ArrayList<String> arrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_lat);
         listView = (ListView) findViewById(R.id.listOfGroup);
+        registerForContextMenu(listView);
         try {
-            //String dat="><Name><Date><Type><Code><Comment><><Name1><Date><Type><Code><Comment><><Name2><Date><Type><Code><Comment><><Name3><Date><Type><Code><Comment><";
             FileOutputStream fileOutputStream = openFileOutput("File2", MODE_PRIVATE);
             deleteFile("File2");
-            //fileOutputStream.write(dat.getBytes());
-            //fileOutputStream.flush();
-            //fileOutputStream.close();
             File file = new File(getApplicationInfo().dataDir+"/files");
             arr = file.list();
         } catch (Exception e) {
@@ -44,18 +51,19 @@ public class MainActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent intent = new Intent(getApplicationContext(),BarcodesActivity.class);
-                intent.putExtra("name",arr[i]);
-                startActivity(intent);
+                    Intent intent = new Intent(getApplicationContext(), BarcodesActivity.class);
+                    intent.putExtra("name", arr[i]);
+                    startActivity(intent);
             }
         });
 
 
+
     }
-    public  void onClick(View view){
-        showAlertDialog();
+    void onClick(View view){
+        showAlertDialogCreate();
     }
-    public void createNewGroup(String name){
+    void createNewGroup(String name){
         try {
             FileOutputStream fileOutputStream = openFileOutput(name, MODE_APPEND);
             fileOutputStream.flush();
@@ -65,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void showAlertDialog() {
+     void showAlertDialogCreate() {
         editText=new EditText(MainActivity.this);
         AlertDialog.Builder dial = new AlertDialog.Builder(MainActivity.this);
         dial.setTitle("Create new group");
@@ -86,4 +94,63 @@ public class MainActivity extends AppCompatActivity {
         });
         dial.show();
     }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.context_menu_group_barcodes, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo contextMenuInfo = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()){
+            case R.id.delete : deleteGroup(arr[contextMenuInfo.position]);
+                break;
+            case R.id.rename : renameGroup(arr[contextMenuInfo.position]);
+                break;
+        }
+        recreate();
+        return super.onContextItemSelected(item);
+    }
+
+    private void renameGroup(final String name) {
+        edtForRename = new EditText(MainActivity.this);
+        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Enter a new name");
+        edtForRename.setHint("New name");
+        builder.setView(edtForRename);
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                renameFile(name, editText.getText().toString());
+            }
+        });
+        builder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.show();
+
+    }
+
+    private void renameFile(String oldName, String newName) {
+        File oldFile = new File(getApplicationInfo().dataDir+"/files/"+oldName);
+        File newFile = new File(getApplicationInfo().dataDir+"/files/"+newName);
+        oldFile.renameTo(newFile);
+    }
+
+    private void deleteGroup(String name) {
+        try {
+            File file = new File(getApplicationInfo().dataDir+"/files/"+name);
+            file.delete();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
